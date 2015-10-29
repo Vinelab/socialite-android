1eb1e8ae89f3c2550d4e80cb406e761eab074401
Nabil Souk <nabil@vinelab.com>
2015-10-29 15:39:03 +0200

    Add Twitter dependency and the login provider

---
+++ app/src/main/java/com/vinelab/android/sociallogin/socialite/twitter/TwitterCredentials.java
@@ -0,0 +1,29 @@
+package com.vinelab.android.sociallogin.socialite.twitter;
+
+import com.vinelab.android.sociallogin.socialite.SocialiteCredentials;
+
+/**
+ * Created by Nabil Souk on 10/29/2015.
+ *
+ * <p>
+ *     Object holding the credentials of a Twitter active session.
+ * </p>
+ */
+public class TwitterCredentials extends SocialiteCredentials {
+    private String token;
+    private String secret;
+
+    public TwitterCredentials(String userId, String token, String secret) {
+        super(userId);
+        this.token = token;
+        this.secret = secret;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+}
---
+++ app/src/main/java/com/vinelab/android/sociallogin/socialite/twitter/TwitterLoginProvider.java
@@ -0,0 +1,155 @@
+package com.vinelab.android.sociallogin.socialite.twitter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.vinelab.android.sociallogin.R;
+import com.vinelab.android.sociallogin.socialite.SocialiteUtils;
+import com.vinelab.android.sociallogin.socialite.listeners.SocialiteLoginListener;
+import com.vinelab.android.sociallogin.socialite.listeners.SocialiteUserStateListener;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by Nabil Souk on 10/28/2015.
+ *
+ * <p>
+ *     Class providing Twitter login functionality.
+ * </p>
+ */
+public class TwitterLoginProvider {
+    private static TwitterLoginProvider twitterLoginProvider;
+    private TwitterAuthClient twitterAuthClient;
+    private SocialiteLoginListener loginListener;
+    private final SocialiteUtils.SOCIALITE_PROVIDER provider = SocialiteUtils.SOCIALITE_PROVIDER.TWITTER;
+
+    private TwitterLoginProvider() {}
+
+    public static TwitterLoginProvider getInstance() {
+        if(twitterLoginProvider == null)    twitterLoginProvider = new TwitterLoginProvider();
+        return twitterLoginProvider;
+    }
+
+    /**
+     * Initializes the SDK with the correspondent Twitter app identifiers.
+     */
+    public void initialize(Context context) {
+        // get consumer credentials from xml
+        String consumerKey = context.getResources().getString(R.string.twitter_consumer_key);
+        String consumerSecret = context.getResources().getString(R.string.twitter_consumer_secret);
+        // initialize the SDK
+        TwitterAuthConfig authConfig =  new TwitterAuthConfig(consumerKey, consumerSecret);
+        Fabric.with(context, new TwitterCore(authConfig));
+        // auth client
+        twitterAuthClient = new TwitterAuthClient();
+    }
+
+    /**
+     * Checks if the user has an active session (still logged in).
+     * @param loginStateListener Callback listener for the user state.
+     */
+    public void isUserLoggedIn(SocialiteUserStateListener loginStateListener) {
+        if(loginStateListener != null) {
+            // check if the user already has an active session
+            TwitterSession twitterSession = TwitterCore.getInstance().getSessionManager().getActiveSession();
+            if(twitterSession != null && twitterSession.getAuthToken() != null) {
+                long userId = twitterSession.getUserId();
+                String token = twitterSession.getAuthToken().token;
+                String secret = twitterSession.getAuthToken().secret;
+                // broadcast result
+                TwitterCredentials credentials = new TwitterCredentials(String.valueOf(userId), token, secret);
+                loginStateListener.onLoggedIn(provider, credentials);
+            }
+            else {
+                loginStateListener.onLoggedOut(provider);
+            }
+        }
+    }
+
+    /**
+     * Triggers the login process of the SDK.
+     * @param activity The activity which is starting the login process.
+     * @param listener The callback listener.
+     */
+    public void login(Activity activity, SocialiteLoginListener listener) {
+        // set listener
+        loginListener = listener;
+        // trigger login
+        twitterAuthClient.authorize(activity, twitterSessionCallback);
+    }
+
+    /**
+     * Called to forward the login results for the TwitterAuthClient.
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(twitterAuthClient != null)   twitterAuthClient.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Login callbacks listener.
+     */
+    Callback<TwitterSession> twitterSessionCallback = new Callback<TwitterSession>() {
+        @Override
+        public void success(Result<TwitterSession> result) {
+            if(result != null) {
+                // check result
+                TwitterAuthToken authToken = result.data.getAuthToken();
+                if(authToken != null) {
+                    long userId = result.data.getUserId();
+                    String token = authToken.secret;
+                    String secret = authToken.secret;
+                    // broadcast result
+                    broadcastLoginSuccess(token, secret, userId);
+                    return;
+                }
+            }
+            // if something wen wrong
+            broadcastLoginError(null);
+        }
+
+        @Override
+        public void failure(TwitterException e) {
+            broadcastLoginError(e != null? e.getMessage(): null);
+        }
+    };
+
+    // provider callbacks //
+
+    /**
+     * Broadcasts a login success flag along with the current session data.
+     * @param token The auth token of the current active session.
+     * @param secret The auth secret of the current active session.
+     * @param userId The Twitter ID of the logged user.
+     */
+    private void broadcastLoginSuccess(String token, String secret, long userId) {
+        if(loginListener != null) {
+            TwitterCredentials credentials = new TwitterCredentials(String.valueOf(userId), token, secret);
+            loginListener.onSuccess(provider, credentials);
+        }
+    }
+
+    /**
+     * Broadcasts a login error flag.
+     * @param error The error message returned by the SDK.
+     */
+    private void broadcastLoginError(@Nullable String error) {
+        if(loginListener != null)   loginListener.onError(provider, error);
+    }
+
+    /**
+     * Broadcasts a login cancelled flag.
+     */
+    private void broadcastLoginCancel() {
+        if(loginListener != null)   loginListener.onCancel(provider);
+    }
+}
